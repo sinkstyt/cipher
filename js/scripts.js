8ee4bcb5cfa4300b958cc0541c2bbc13951a0860
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-19 22:26:46 -0400

    add 404.html, LICENSE.txt, README.md, favicon.ico, humans.txt, index.html, dir js/, robots.txt, site.webmanifest, tile-wide.png, tile.png and first four functions in scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+let sentence = prompt("Please give me a sentence you would like to scramble:");
+console.log(sentence);
+
+function capitalize1stAndLast(inputString) {
+  if (typeof inputString == "string") {
+    // declare a new variable outputString and capture a call .toUpperCase on inputString's first index value
+    let outputString += inputString[0].toUpperCase();
+    // capitalize the last letter of inputString
+    if (typeof inputString[-1] !== "." || "?" || "!") {
+      let lastLetter = inputString[-1];
+    } else {
+      let lastLetter = inputString[-2];
+    }
+    lastLetter = lastLetter.toUpperCase():
+    outputString += lastLetter;
+    return outputString;
+  } else {
+    prompt("Please provide your sentence some words.")
+  }
+}
+
+function reverseTwoCapitals(veryShortString) {
+  let output = veryShortString[1];
+  output += veryShortString[0];
+  return output;
+}
+function attachTwoCapsAtEnd() {
+  let newTwoCaps = reverseTwoCapitals(capitalize1stAndLast(sentence));
+  let output = sentence + newTwoCaps;
+  return output;
+}
+function startWithMiddleLetter(str) {
+  // NOPE, THIS PART IS NOT WHAT WAS MEANT IN THE PROMPT:    (  find every index whose value is " " and remove each and every " " found using RegExp
+  // str = str.replace(/\s+/g, '');  )
+
+  // Divide str.length by 2 and round down if necessary
+  let halfLength = Math.floor(str.length / 2);
+  // output the letter at this halfway point in the letter count
+  let middleLetter = str[halfLength];
+  str = middleLetter + str;
+  return str;
+}
+let scrambledSentence = startWithMiddleLetter(sentence) + attachTwoCapsAtEnd();
+// Now reverse scrambledSentence:
